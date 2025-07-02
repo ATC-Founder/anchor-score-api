aaca013843a1693cf266d377e33cfd57656daf5d
Johnnie Griffing <johnniegriffing@Mac.attlocal.net>
2025-07-01 22:59:45 -0700

    Add health check route

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
 from fastapi import FastAPI
-from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "AnchorScore API is live!"}
-
 @app.get("/healthz")
 def health_check():
-    return JSONResponse(content={"status": "ok"})
+    return {"status": "ok"}
